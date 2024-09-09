bdad9d3fcb28bc43f45b1aa1466ff0396c770b6e
Bryden Basaran <bbasaran@misoenergy.org>
2024-09-09 14:46:46 -0400

    Initial-Commit

---
+++ kessel-run/src/main/java/org/misoenergy/hackday/kessel_run/KesselRunApplication.java
@@ -0,0 +1,13 @@
+package org.misoenergy.hackday.kessel_run;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KesselRunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KesselRunApplication.class, args);
+	}
+
+}
---
+++ kessel-run/src/test/java/org/misoenergy/hackday/kessel_run/KesselRunApplicationTests.java
@@ -0,0 +1,13 @@
+package org.misoenergy.hackday.kessel_run;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KesselRunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
